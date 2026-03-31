07c50750fe5b76f0eb65f1e49348786500a1aa94
JansiRani2002 <jmandhula@gmail.com>
2026-04-01 01:02:08 +0530

    fix: handle edge case when k > number of input words

--- permutation.py
+++ permutation.py
@@ -1,11 +1,21 @@
 from randomized_queue import RandomizedQueue
+
 rq = RandomizedQueue()
+
+# Read words from input
 items = input("Enter words separated by space: ").split()
 
+# Enqueue all words into RandomizedQueue
 for item in items:
     rq.enqueue(item)
 
+# Read k
 k = int(input("How many random items to print? "))
 
+# Validate k
+if k > rq.size():
+    raise ValueError("k cannot be greater than number of input words")
+
+# Print k random items
 for _ in range(k):
     print(rq.dequeue())
